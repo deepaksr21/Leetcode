c6d755cd855d8a0ea3daba31feef9b95e0594f0f
deepaksr21 <deepaksingh211105@gmail.com>
2026-09-11 18:01:41 +0530

    Add LC-802 Solution

---
+++ Graphs/findEventaulSafeStates-802.cpp
@@ -0,0 +1,36 @@
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    bool dfs(int node, vector<vector<int>>& graph, vector<int>& state) {
+        if (state[node] == 1)
+            return false;
+
+        if (state[node] == 2)
+            return true;
+
+        state[node] = 1;
+
+        for (int next : graph[node]) {
+            if (!dfs(next, graph, state))
+                return false;
+        }
+
+        state[node] = 2;
+        return true;
+    }
+
+    vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
+        int n = graph.size();
+        vector<int> state(n);
+        vector<int> ans;
+
+        for (int i = 0; i < n; i++) {
+            if (dfs(i, graph, state))
+                ans.push_back(i);
+        }
+
+        return ans;
+    }
+};
